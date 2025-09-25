e18e9f905a551081d9ad65973dd6b6323ab42335
AngeloGauto <67487570+AngeloGauto@users.noreply.github.com>
2025-09-25 13:23:29 +0000

    inserido códiigo do desafio mestre do batalha naval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,55 +1,94 @@
 #include <stdio.h>
 
+#define LINHA 10
+#define COLUNA 10
+
 int main (){
 
-    //declara matriz com valores inseridos para criar tabuleiro
-    int tabuleiro [10] [10];
+    //Realizei a inserção das formas geometricas usando 3 lógicas diferentes para aprender, entender os loops e como 
+    //eles interagem entre si, mesmo a ultima que foi feita no octoedro ser a mais eficiente permitindo uma entrada
+    //simples de usuario com o local facilmente, mantive as outras.
 
-    for (int i = 0; i < 10; i++)
+    //declara matriz com valores inseridos para criar tabuleiro
+    int tabuleiro [LINHA] [COLUNA];
+    //declara variaveis para uso de controle das formas geometricas
+    int contaCone = 0;
+    int contaCruz = 0;
+    int posInicialOcto = 6;
+    //declara letra para uso de orientação do tabuleiro
+    char letra = 'A';
+
+
+    //Preenche 0 no tabuleiro
+    for (int i = 0; i < LINHA; i++)
     {
-        for (int j = 0; j < 10; j++)
+        for (int j = 0; j < COLUNA; j++)
         {
            tabuleiro [i][j] = 0;
         }
         
     }
     
-    char * coluna [10] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
-
-    //insere navio 1 horizontal posicionamento for aninhado
-    for (int i = 2; i == 2; i++)
+    //mostra triangulo no prompt
+    for (int i = 2; i < LINHA; i++)
     {
-        for (int j = 3; j <= 5; j++)
+        for (int j = 2, y = 2; j < COLUNA; j++, y--)
         {
-            tabuleiro [i][j] = 3;
-        } 
+            if (j <= i && contaCone <= 5)
+            {
+                 tabuleiro [i][j] = 5;
+                 tabuleiro [i][y] = 5;
+                 ++contaCone;
+            }
+        }   
     }
 
-    //insere navio 2 vertical posicionamento for aninhado
-    for (int j = 4; j == 4; j++)
+    //Exibe cruz no prompt
+    for (int i = 2; i < LINHA; i++)
     {
-        for (int i = 6; i <= 8; i++)
+        for (int j = 7, y = 7; j < COLUNA; j++, y-- )
         {
-            tabuleiro [i][j] = 3;
+            //horizontal
+
+            if (contaCruz <=2)
+            {
+                tabuleiro [i][j] = 8;
+                tabuleiro [i][y] = 8;
+                ++contaCruz;
+
+            //vertical   
+            }else if (contaCruz <=3)
+            {
+                y=7;
+                tabuleiro [i-2][y] = 8;
+                tabuleiro [i][y] = 8;
+                ++contaCruz;
+            }
+            
+        }
+
+    }
+
+    //Exibe octaedro no prompt
+    for (int i = 0; i < LINHA; i++)
+    {
+        for (int j = 0; j < COLUNA; j++)
+        {
+            
+            if (i==posInicialOcto && j==posInicialOcto)
+            {
+                //horizontal
+                tabuleiro [i][j] = 7;
+                tabuleiro [i][j-1] = 7;
+                tabuleiro [i][j+1] = 7;
+
+                //vertical
+                tabuleiro [i-1][j] = 7;
+                tabuleiro [i+1][j] = 7;
+            }
         }
     }
 
-    //Exibe navio na primeira diagonal posicionamento por for
-     for (int i = 0, j = 0, cont1 = 0; i < 10 && cont1 < 3 ; i++, j++)
-    {
-       tabuleiro[i][j+5] = 3; //exibe navio movimentando as casas
-       ++cont1;
-    }
-    
-    //Exibe navio na segunda diagonal posicionamento por for
-     for (int i = 0, cont2 = 0; i < 10 && cont2 < 3; i++)
-    {
-       int j = 9 - i;
-       tabuleiro[i+4][j] = 3;//exibe navio movimentando as casas
-       ++cont2;
-    }
-
-    
     //exibe nome do jogo
     printf ("-----TABULEIRO BATALHA NAVAL-----\n");
 
@@ -57,22 +96,23 @@ int main (){
     printf("    ");
 
     //cria orientação do tabuleiro de A até J para colunas
-    for (int i = 0; i < 10; i++)
+    for (int i = 0 ; i < COLUNA; i++)
     {
-        printf("%s  ", coluna [i]);
+       printf("%c  ", letra);
+       ++letra;
     }
     printf ("\n");
 
 
     //for para trocar linha de exibição do tabuleiro
-    for (int i = 0, x=1; i < 10; i++, x++)
+    for (int i = 0, x=1; i < LINHA; i++, x++)
 
     {
-        //exibe numero de orientação de linhas
+        //exibe numero de orientação de linhas laterais a esquerda de 1 a 10
         printf("%2d  ", x);
 
         //for que mostra tabuleiro e navios
-        for (int j = 0; j < 10; j++)
+        for (int j = 0; j < COLUNA; j++)
         {
             printf ("%d  ",tabuleiro[i] [j]);
         }
